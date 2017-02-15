c2b64386ef792f6fcea418e1df3165fffe2f6913
ShaggyBlarney <kearney_@hotmail.com>
2017-02-14 17:05:46 -0800

    Added ordered list page features

---
+++ application/controllers/Views.php
@@ -0,0 +1,24 @@
+<?php
+
+class Views extends Application {
+    
+    public function index() {
+        $this->data['pagetitle'] = 'Ordered TODO List';
+        $tasks = $this->tasks->all();
+        $this->data['content'] = 'Ok';
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
+        
+        $this->render('template_secondary');
+    }
+    
+    function makePrioritizedPanel($tasks) {
+        $parms = ['display_tasks' => []];
+        return $this->parser->parse('by_priority', $parms, true);
+    }
+    
+    function makeCategorizedPanel($tasks) {
+        $parms = ['display_tasks' => []];
+        return $this->parser->parse('by_category', $parms, true);
+    }
+}
